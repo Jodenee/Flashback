9e94ca6803222e9ed929a643f15fb7de15bffec7
Jodenee <81998397+Jodenee@users.noreply.github.com>
2026-01-21 18:04:02 +0100

    Add models
    adds a models folder to contain all model classes

---
+++ lib/models/flashback.dart
@@ -0,0 +1,7 @@
+class Flashback {
+  final String text;
+  final double latitude;
+  final double longitude;
+
+  Flashback({required this.text, required this.latitude, required this.longitude});
+}
